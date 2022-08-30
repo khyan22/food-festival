6db05953d531ee5917480efbe085ffd42f568af2
khyan22 <khyanalvarez@gmail.com>
2022-08-29 22:50:32 -0400

    repush

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,41 @@
 const path = require('path');
 const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-  entry: './assets/js/script.js',
+  entry: {
+    app: './assets/js/script.js',
+    events: './assets/js/events.js',
+    schedule: './assets/js/schedule.js',
+    tickets: './assets/js/tickets.js'
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.bundle.js'
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules:[
+      {
+        test: /\.jpg$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false,
+              name (file) {
+                return '[path][name].[ext]'
+              },
+              publicPath: function(url) {
+                return url.replace('./','/assets/')
+              }
+            }
+          },
+          {
+            loader: 'image-webpack-loader'
+          }
+        ]
+      }
+    ]
   },
   mode: 'development',
   plugins: [
@@ -13,5 +43,9 @@ module.exports = {
       $: 'jquery',
       jQuery: 'jquery'
     }),
+    new BundleAnalyzerPlugin({
+      //*switch to static to see report
+      analyzerMode: "disable", //the report outputs an html file in dist
+    })
   ],
 };
