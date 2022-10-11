9763c09b56226da2f43802bd6a82a1c15c8cc950
khyan22 <khyanalvarez@gmail.com>
2022-10-10 20:12:58 -0400

    turned app into pwa

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = {
   entry: {
@@ -46,6 +47,21 @@ module.exports = {
     new BundleAnalyzerPlugin({
       //*switch to static to see report
       analyzerMode: "disable", //the report outputs an html file in dist
+    }),
+    new WebpackPwaManifest({
+      name: "Food Event",
+      short_name: 'Foodies',
+      description: 'An app that allows you to view upcoming good events.',
+      start_url: '../index.html',
+      background_color: '#01579b',
+      theme_color: '#ffffff',
+      fingerprints: false,
+      inject: false,
+      icons:[{
+        src: path.resolve('assets/img/icons/icon-512x512.png'),
+        sizes: [96, 128, 192, 256, 384, 512],
+        destination: path.join('assets', 'icons')
+      }]
     })
   ],
 };
